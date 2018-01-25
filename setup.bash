c3ddae2b30d0a94b3a55a74e325abfeb7c59125f
trilaiclub <trilaiclub@gmail.com>
2018-01-25 11:14:26 +0530

    setup changes to run forever

--- setup.bash
+++ setup.bash
@@ -1,1 +1,2 @@
 export SOLR_HOME=/home/ubuntu/tac/solr-bucket/server/solr
+forever start --verbose --append --uid \"solr-bucket\" --watch --watchDirectory $SOLR_HOME/bin/start --spinSleepTime 1000ms
